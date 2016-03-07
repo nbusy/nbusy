01561faac69a43bf747f696b15edfb19070bc6f6
Teoman Soygul <tsoygul@gmail.com>
2016-03-07 12:15:38 +0100

    expand default commands

--- main.go
+++ main.go
@@ -1,13 +1,30 @@
 package main
 
 import (
+	"flag"
 	"log"
 
 	"github.com/titan-x/titan"
 )
 
-func main() {
+const (
+	addr = "127.0.0.1:3000"
+)
 
+var (
+	run   = flag.Bool("run", false, "Start an NBusy server at address: "+addr)
+	caddr = flag.String("addr", addr, "Specifies a network address to start the server on. If not specific, default will be used: "+addr)
+)
+
+func main() {
+	flag.Parse()
+
+	switch {
+	case *run:
+		startServer(*caddr)
+	default:
+		flag.PrintDefaults()
+	}
 }
 
 func startServer(addr string) {
