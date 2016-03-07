445f4db09be69b842d9dd1c3b93bba572f89b3dd
Teoman Soygul <tsoygul@gmail.com>
2016-03-07 01:11:33 +0100

    cleanup for go switch

--- client/modules/common/common.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-/**
- * Module definition for common components used by all other app modules.
- */
-
-angular.module('nbusy.common', []);
--- client/modules/messages/messages-controller.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-
-angular.module('nbusy.messages').controller('MessagesCtrl', function ($scope) {
-});
