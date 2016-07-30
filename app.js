04144609e406b9474516d8c37c28c65c921656a1
Teoman Soygul <tsoygul@gmail.com>
2016-07-30 21:38:43 +0300

    add bluebird note

--- app.js
+++ app.js
@@ -3,3 +3,5 @@ require('babel-register');
 var asyncFun = require('./asyncFun');
 
 console.log(asyncFun.asyncResult);
+
+// todo: https://medium.com/@rdsubhas/es6-from-callbacks-to-promises-to-generators-87f1c0cd8f2e#.x9bakv1ir
