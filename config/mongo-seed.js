f7086f133a3ba21fb65f74a846f7cd4b56e3addc
Teoman Soygul <tsoygul@gmail.com>
2016-08-07 02:00:56 +0300

    add jwt module

---
+++ config/mongo-seed.js
@@ -0,0 +1,83 @@
+const mongo = require('./mongo'),
+  config = require('./config'),
+  ObjectID = mongo.ObjectID;
+
+/**
+ * Populates the database with seed data.
+ * @param overwrite Overwrite existing database even if it is not empty.
+ */
+function *seed(overwrite) {
+  var count = yield mongo.users.count({}, {limit: 1});
+  if (overwrite || count === 0) {
+    // first remove any leftover data in collections
+    var collerrmsg = 'ns not found' /* indicates 'collection not found' error in mongo which is ok */;
+    for (var collection in mongo) {
+      if (mongo[collection].drop) {
+        try {
+          yield mongo[collection].drop();
+        } catch (err) {
+          if (err.message !== collerrmsg) {
+            throw err;
+          }
+        }
+      }
+    }
+
+    // now populate collections with fresh data
+    yield mongo.counters.insert({_id: 'userId', seq: users.length});
+    yield mongo.users.insert(users);
+    yield mongo.posts.insert(posts);
+  }
+}
+
+// declare seed data
+const users = [
+  {
+    _id: 1,
+    email: 'morgan@koan.herokuapp.com',
+    password: config.app.pass,
+    name: 'Morgan the Almighty',
+  },
+  {
+    _id: 2,
+    email: 'chuck@koan.herokuapp.com',
+    password: config.app.pass,
+    name: 'Chuck Norris',
+
+  },
+];
+
+
+var now = new Date();
+function getTime(h) {
+  return new Date(new Date(now).setHours(now.getHours() + h));
+}
+
+var posts = [
+  {
+    _id: new ObjectID(),
+    from: {_id: 1, name: 'Morgan the Almighty', picture: '/api/users/1/picture'},
+    message: 'Hi there! This is a sample post demonstrating a KOAN app. KOAN is a simple boilerplate for building full-stack JavaScript Web applications using Koa, AngularJS, and Node.js. It utilizes WebSockets to provide real-time communication between servers and clients, and MongoDB is used for data persistence. There are also numerous Grunt tasks pre-bundled and configured to facilitate development and testing. You can open this site in multiple browser tabs and post something to see how real-time communication works. You can also browse the project’s GitHub page to start building KOAN apps yourself.',
+    createdTime: getTime(-97),
+    updatedTime: getTime(-24),
+    comments: [
+      {
+        _id: new ObjectID(),
+        from: {_id: 2, name: 'Chuck Norris', picture: '/api/users/2/picture'},
+        createdTime: getTime(-26),
+        message: 'Also remember that, if you can read this, you are within range of Chuck!'
+      },
+      {
+        _id: new ObjectID(),
+        from: {_id: 1, name: 'Morgan the Almighty', picture: '/api/users/1/picture'},
+        createdTime: getTime(-24),
+        message: 'Ow yeah!'
+      }
+    ]
+  }
+];
+
+// export seed data and seed function
+seed.users = users;
+seed.posts = posts;
+module.exports = seed;
---
+++ test/mongo.js
@@ -0,0 +1,3 @@
+// todo: add test for getNextSequence()
+
+// todo: http://mongodb.github.io/node-mongodb-native/2.2/reference/ecmascript6/crud/
