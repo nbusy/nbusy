cde9f45409440ec31a26da057c898e12fcd7d294
Teoman Soygul <tsoygul@gmail.com>
2016-08-09 19:50:59 +0300

    add Jest as test runner

--- __tests__/mongo-test.js
+++ __tests__/mongo-test.js
@@ -1,3 +1,11 @@
 // todo: add test for getNextSequence()
 
 // todo: http://mongodb.github.io/node-mongodb-native/2.2/reference/ecmascript6/crud/
+
+// jest.enableAutomock();
+
+describe('sum', () => {
+  it('adds 1 + 2 to equal 3', () => {
+
+  });
+});
