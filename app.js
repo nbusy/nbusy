35f73a5ea8aeac2b1c0cc2a00188572b603f0090
Teoman Soygul <tsoygul@gmail.com>
2016-09-19 15:53:57 +0200

    rename http server to be more explicit

--- app.js
+++ app.js
@@ -2,23 +2,23 @@
  * NBusy application entry point.
  */
 
-const http = require('./config/http')
+const httpServer = require('./config/http-server')
 const config = require('./config/config')
 const mongo = require('./config/mongo')
 const ws = require('./config/ws')
 
 /**
- * Initializes a new NBusy server..
+ * Initializes a new NBusy server.
  * @param overwriteDB Overwrite existing database with the seed data. Useful for testing environment.
  */
 exports.init = async function (overwriteDB) {
   await mongo.connect(config.mongo.url)
   await mongo.seed(overwriteDB)
 
-  exports.server = http.server
+  exports.server = httpServer.server
   ws.listen(exports.server)
   if (config.app.env !== 'test') {
-    console.log('NBusy listening on port ' + config.app.port)
+    console.log('NBusy server listening on port ' + config.app.port)
   }
 }
 
---
+++ config/http-server.test.js
@@ -0,0 +1,25 @@
+const fetch = require('node-fetch')
+const httpServer = require('./http-server')
+
+describe('httpServer', () => {
+  beforeEach(async () => {
+    await httpServer.listen()
+  })
+
+  afterEach(async () => {
+    await httpServer.close()
+  })
+
+  it('opens/closes synchronously', async () => {
+    expect(httpServer.server.listening).toBe(true)
+    await httpServer.close()
+    expect(httpServer.server.listening).toBe(false)
+    await httpServer.listen()
+    expect(httpServer.server.listening).toBe(true)
+  })
+
+  it('replies all requests with 426', async () => {
+    const res = await fetch('http://localhost:3001')
+    expect(res.status).toBe(426)
+  })
+})
