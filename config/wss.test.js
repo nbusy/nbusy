82d4bd6f48a45845d25d044bae0e27b60b594f04
Teoman Soygul <tsoygul@gmail.com>
2016-09-20 15:06:51 +0200

    bring back ws implementation

--- config/wss.test.js
+++ config/wss.test.js
@@ -1,22 +1,37 @@
-const wss = require('./wss')
-const config = require('./config')
+const WebSocket = require('ws')
+const jwt = require('jsonwebtoken')
+const config = require('./../config/config')
+const ws = require('./wss')
+const seed = require('./../config/seed')
 
-describe('wss', () => {
-  it('connects', () => {
-    var WebSocket = require('ws');
-    var ws = new WebSocket(`http://localhost:${config.app.port}`);
+const url = `ws://127.0.0.1:${config.app.port}`
 
-    ws.on('open', function open() {
-      ws.send('something');
-    });
+describe('ws', () => {
+  beforeEach(() => {
+    ws.listen(null, config.app.port)
+  })
 
-    ws.on('message', function(data, flags) {
-      console.log('client received: %s', data)
-    });
+  afterEach(() => {
+    ws.wss.close()
+  })
 
-    ws.close()
+  it('connect', () => {
+    const token = jwt.sign({id: seed.users[0]._id}, config.app.pass)
+    const ws = new WebSocket(`${url}?access_token=${token}`)
 
-    wss.close()
+    const ping = new Promise(resolve => {
+      ws.on('open', function open () {
+        ws.send('ping')
+        ping.resolve()
+      })
+    })
+
+    const msg = new Promise(resolve => {
+      ws.on('message', function (data, flags) {
+        msg.resolve()
+      })
+    })
+
+    return Promise.all([ping, msg])
   })
 })
-
