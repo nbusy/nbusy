1cc92a6eb02faab518c7eb03c6d49c68d822b2c1
Teoman Soygul <tsoygul@gmail.com>
2016-09-20 16:10:49 +0200

    get the ws tests passing

--- config/wss.test.js
+++ config/wss.test.js
@@ -1,33 +1,36 @@
 const WebSocket = require('ws')
 const jwt = require('jsonwebtoken')
 const config = require('./../config/config')
-const ws = require('./wss')
+const wss = require('./wss')
 const seed = require('./../config/seed')
 
 const url = `ws://127.0.0.1:${config.app.port}`
 
 describe('ws', () => {
   beforeEach(() => {
-    ws.listen(null, config.app.port)
+    wss.listen(null, config.app.port)
   })
 
   afterEach(() => {
-    ws.wss.close()
+    // todo: ws.wss -> wss only
+    wss.wss.close()
   })
 
   it('connect', () => {
-    const token = jwt.sign({id: seed.users[0]._id}, config.app.pass)
-    const ws = new WebSocket(`${url}?access_token=${token}`)
+    const token = jwt.sign({id: seed.users[0]._id}, config.app.secret)
+    const ws = new WebSocket(`${url}/?access_token=${token}`)
 
     const ping = new Promise(resolve => {
       ws.on('open', function open () {
         ws.send('ping')
         resolve()
+        wss.notify('wow', {yeah: 'hiii'})
       })
     })
 
     const msg = new Promise(resolve => {
       ws.on('message', function (data, flags) {
+        console.log(`client received: ${data}`)
         resolve(data)
       })
     })
