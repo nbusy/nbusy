f4eea22ae437816cd3fdee183610c76e5ce30133
Teoman Soygul <tsoygul@gmail.com>
2016-09-20 10:51:23 +0200

    adjust ws tests for async

--- config/ws.test.js
+++ config/ws.test.js
@@ -1,8 +1,10 @@
 const WebSocket = require('ws')
+const jwt = require('jsonwebtoken')
 const config = require('./config')
 const ws = require('./ws')
+const seed = require('./seed')
 
-const url = 'ws://127.0.0.1:' + config.app.port
+const url = `ws://127.0.0.1:${config.app.port}`
 
 describe('ws', () => {
   beforeEach(() => {
@@ -14,14 +16,21 @@ describe('ws', () => {
   })
 
   it('connect', () => {
-    const ws = new WebSocket(url)
+    const token = jwt.sign({id: seed.users[0]._id}, config.app.pass)
+    const ws = new WebSocket(`${url}?access_token=${token}`)
 
+    const ping = new Promise(() => {})
     ws.on('open', function open() {
       ws.send('ping');
+      ping.resolve()
     });
 
+    const msg = new Promise(() => {})
     ws.on('message', function(data, flags) {
       console.log(data)
+      msg.resolve()
     });
+
+    return Promise.all([ping, msg])
   })
 })
