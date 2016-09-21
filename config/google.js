e501c882e4d148c32e402039e9c3b329807f4a1b
Teoman Soygul <tsoygul@gmail.com>
2016-09-21 16:02:17 +0200

    throw on auth error

--- config/google.js
+++ config/google.js
@@ -2,5 +2,9 @@ const fetch = require('node-fetch')
 
 exports.tokeninfo = async (token) => {
   const res = await fetch(`https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=${token}`)
-  return res.status < 400
+  if (res.status >= 300) {
+    throw 'authentication failed'
+  }
+
+  return await res.json()
 }
