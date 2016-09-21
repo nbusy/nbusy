fdd48b8064f8ede0446ec79d59ebb1942a1e0520
Teoman Soygul <tsoygul@gmail.com>
2016-09-21 16:12:52 +0200

    google: optionally persist user details

--- config/google.js
+++ config/google.js
@@ -1,10 +1,17 @@
 const fetch = require('node-fetch')
 
-exports.tokeninfo = async (token) => {
+exports.tokeninfo = async (token, db) => {
   const res = await fetch(`https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=${token}`)
   if (res.status >= 300) {
     throw new Error('authentication failed')
   }
 
-  return await res.json()
+  body = await res.json()
+
+  if (db) {
+    // persist user details
+    return body
+  }
+
+  return body
 }
