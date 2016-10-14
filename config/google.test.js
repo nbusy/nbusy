f0eb46bed093ea0e8a7101fe43747cd1d0de8799
Teoman Soygul <tsoygul@gmail.com>
2016-10-14 21:48:44 +0200

    google: add sample usage of future async expect

--- config/google.test.js
+++ config/google.test.js
@@ -2,7 +2,7 @@ const google = require('./google')
 
 describe('google', () => {
   it('tokeninfo', async () => {
-    // todo: https://github.com/facebook/jest/issues/1377
+    // todo: https://github.com/facebook/jest/issues/1377  --  await expect(async () => await google.tokeninfo('falsetoken')).toThrow(new Error('authentication failed'))
     let gotErr = false
     try {
       await google.tokeninfo('falsetoken')
